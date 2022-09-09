607847c6c77a0614172adcea0727bd7fda27fd36
Sourav Banerjee <rjsourav88@gmail.com>
2022-09-09 15:40:44 +0530

    Final commit

---
+++ src/Components/Search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Search(props) {
+    return (
+        <div className="input-container display-flex">
+            <div className='input-wrapper display-flex'>
+                <div className='search-icon'>
+                    <i className="bi bi-search"></i>
+                </div>
+                <input
+                    className='search-input'
+                    type={"text"}
+                    placeholder={props.placeholder}
+                    onChange={(e) => props.data(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
